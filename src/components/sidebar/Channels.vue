18632079abb1c18cc867895fbd52c8876a1ae8f6
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-02-28 09:02:30 +0100

    openchannelmodal

--- src/components/sidebar/Channels.vue
+++ src/components/sidebar/Channels.vue
@@ -1,19 +1,60 @@
 <template>
 
     <div class="channels__container">
-        <h2 class="ui inverted center aligned header">Channels <i class="add circle icon add_channel" ></i></h2>
+      <h2 class="ui inverted center aligned header">Channels <i class="add circle icon add_channel" @click="openChannelModal"></i></h2>
         <div class="channels__list">
           <ul>
             <li class="channels__item">affichage liste</li>
           </ul>
         </div>
 
+        <div class="ui basic modal" id="channelModal">
+          <div class="ui icon header">
+            Ajouter un Channel
+        </div>
+
+        <div class="content">
+          <div class="ui inverted form">
+            <div class="field">
+              <label for="new_channel">Nom du Channel</label>
+                <input type="text" name="new_channel" v-model="new_channel" id="new_channel">
+            </div>
+
+            <div class="ui error message">
+
+            </div>
+          </div>
+        </div>
+
+          <div class="actions">
+            <div class="ui red basic cancel inverted button">
+              <i class="remove icon"></i>Annuler
+            </div>
+            <div class="ui green inverted button">
+              <i class="checkmark icon"></i>Ajouter
+            </div>
+        </div>
+      </div>
+    </div>
+
 </template>
 
 <script>
 
     export default {
-        name: 'channels'
+        name: 'channels',
+        data () {
+          return {
+            channels: [],
+            new_channel: '',
+            errors: []
+          }
+        },
+        methods: {
+          openChannelModal () {
+            $("#channelModal").modal('show')
+        }
+      }
     }
 </script>
 
